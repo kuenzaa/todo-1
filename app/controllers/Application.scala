7bbd5721bbc751d0b2d5c7940f0959e6aea8e15e
Charlie Strawn <cdstrawn@gmail.com>
2014-02-03 19:27:57 -0600

    finish todolist sample scala/play app

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,9 +16,20 @@ object Application extends Controller {
     Ok(views.html.index(Task.all(), taskForm))
   }
 
-  def newTask() = play.mvc.Results.TODO
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
 
-  def deleteTask(id: Long) = play.mvc.Results.TODO
+  def deleteTask(id: Long) = Action {
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
+  }
 
   val taskForm = Form(
     "label" -> nonEmptyText
